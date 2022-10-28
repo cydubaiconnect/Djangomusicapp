a6c2b118b02523d15decc2569c9a048cb6229c53
Cy <cyriacusdonpedro41@gmail.com>
2022-10-28 12:04:44 +0100

    added model

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,25 @@
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+
+
+class Album(models.Model):
+    artiste = models.Charfield(max_length=250)
+    album_title = models.Cahrfield(max_length=500)
+    lyrics = models.CharField(max_length=100)
+    song = models.CharField(max_length=1000)
+    
+    
+    class CommonInfo(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+class musician(CommonInfo):
+    home_group = models.CharField(max_length=5)
+    
+        
